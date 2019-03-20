0df6494f9331cba2d1b068f258b7354c279c6fa2
Mikolaj Zywczok <mikolaj.zywczok@gmail.com>
2019-03-20 11:20:16 +0100

    Production #1 - basic + firebase database + routing.

---
+++ build/precache-manifest.95d89c5b124a0bea80e2fc688219beac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c348f88ae82e9ad105cc",
+    "url": "/static/js/main.c08c2716.chunk.js"
+  },
+  {
+    "revision": "670bcb7b6f490a611c8d",
+    "url": "/static/js/2.32d45251.chunk.js"
+  },
+  {
+    "revision": "c348f88ae82e9ad105cc",
+    "url": "/static/css/main.b1a63af3.chunk.css"
+  },
+  {
+    "revision": "32f5b918469d07df444806adaed6c564",
+    "url": "/index.html"
+  }
+];
